e724a7f74ea97a3df6efb56f508dedfcf9af9eee
smilejke <smilejke1@mail.ru>
2019-11-08 13:38:27 +0300

    refactor

--- all_together/scripts.js
+++ all_together/scripts.js
@@ -3,123 +3,121 @@ let doc = document;
 let attempt = 3;
 
 let validate = () => {
-  let username = doc.getElementById("username").value;
-  let password = doc.getElementById("password").value;
-  if (username == "Smilejke" && password == "Admin") {
-    alert("Login successfully");
-    let get_div_and_make_invisible = doc.getElementById("dissap");
+  let username = doc.getElementById('username').value;
+  let password = doc.getElementById('password').value;
+  if (username == 'Smilejke' && password == 'Admin') {
+    alert('Login successfully');
+    let get_div_and_make_invisible = doc.getElementById('dissap');
     doc.body.removeChild(get_div_and_make_invisible);
 
-    let get_div_and_make_visible = doc.getElementById("main_content_div");
-    get_div_and_make_visible.classList.remove("visibility");
+    let get_div_and_make_visible = doc.getElementById('main_content_div');
+    get_div_and_make_visible.classList.remove('visibility');
 
     return false;
   } else {
     attempt--;
-    alert("You have left " + attempt + " attempt");
+    alert('You have left ' + attempt + ' attempt');
 
     if (attempt == 0) {
-      document.getElementById("username").disabled = true;
-      document.getElementById("password").disabled = true;
-      document.getElementById("submit").disabled = true;
+      document.getElementById('username').disabled = true;
+      document.getElementById('password').disabled = true;
+      document.getElementById('submit').disabled = true;
       return false;
     }
   }
 };
 
-doc.onload =
-  (login_form = () => {
-    let global_main_for_dissapiar = doc.createElement('div');
-    global_main_for_dissapiar.id = "dissap";
-    doc.body.appendChild(global_main_for_dissapiar);
+doc.onload = (login_form = () => {
+  let global_main_for_dissapiar = doc.createElement('div');
+  global_main_for_dissapiar.id = 'dissap';
+  doc.body.appendChild(global_main_for_dissapiar);
 
-    let main_login_div = doc.createElement('div');
-    main_login_div.classList.add("main");
-    global_main_for_dissapiar.appendChild(main_login_div);
+  let main_login_div = doc.createElement('div');
+  main_login_div.classList.add('main');
+  global_main_for_dissapiar.appendChild(main_login_div);
 
-    let h2_in_login_form = doc.createElement('h2');
-    h2_in_login_form.innerHTML = "The system of calculating earnings of an employee";
-    main_login_div.appendChild(h2_in_login_form);
+  let h2_in_login_form = doc.createElement('h2');
+  h2_in_login_form.innerHTML = 'The system of calculating earnings of an employee';
+  main_login_div.appendChild(h2_in_login_form);
 
-    let login_form = doc.createElement('form');
-    login_form.id = "form_id";
-    login_form.method = "post";
-    login_form.name = "myform";
-    main_login_div.appendChild(login_form);
+  let login_form = doc.createElement('form');
+  login_form.id = 'form_id';
+  login_form.method = 'post';
+  login_form.name = 'myform';
+  main_login_div.appendChild(login_form);
 
-    let label_userName = doc.createElement('label');
-    label_userName.innerHTML = "User Name :";
-    login_form.appendChild(label_userName);
+  let label_userName = doc.createElement('label');
+  label_userName.innerHTML = 'User Name :';
+  login_form.appendChild(label_userName);
 
-    let input_text = doc.createElement('input');
-    input_text.type = "text";
-    input_text.name = "username";
-    input_text.id = "username";
-    input_text.className = "input_text";
-    login_form.appendChild(input_text);
+  let input_text = doc.createElement('input');
+  input_text.type = 'text';
+  input_text.name = 'username';
+  input_text.id = 'username';
+  input_text.className = 'input_text';
+  login_form.appendChild(input_text);
 
-    let label_userName2 = doc.createElement('label');
-    label_userName2.innerHTML = "Password :";
-    login_form.appendChild(label_userName2);
+  let label_userName2 = doc.createElement('label');
+  label_userName2.innerHTML = 'Password :';
+  login_form.appendChild(label_userName2);
 
-    let input_password = doc.createElement('input');
-    input_password.type = "password";
-    input_password.name = "password";
-    input_password.id = "password";
-    input_password.className = "input_text";
-    login_form.appendChild(input_password);
+  let input_password = doc.createElement('input');
+  input_password.type = 'password';
+  input_password.name = 'password';
+  input_password.id = 'password';
+  input_password.className = 'input_text';
+  login_form.appendChild(input_password);
 
-    let login_button = doc.createElement('input');
-    login_button.type = "button";
-    login_button.value = "Login";
-    login_button.id = "submit";
-    login_button.className = "admin_button";
-    login_button.onclick = validate;
-    login_form.appendChild(login_button);
+  let login_button = doc.createElement('input');
+  login_button.type = 'button';
+  login_button.value = 'Login';
+  login_button.id = 'submit';
+  login_button.className = 'admin_button';
+  login_button.onclick = validate;
+  login_form.appendChild(login_button);
 
-    let span_text = doc.createElement('span');
-    main_login_div.appendChild(span_text);
+  let span_text = doc.createElement('span');
+  main_login_div.appendChild(span_text);
 
-    let bold_text1 = doc.createElement('b');
-    bold_text1.className = "note";
-    bold_text1.innerHTML = "Note:";
-    span_text.appendChild(bold_text1);
+  let bold_text1 = doc.createElement('b');
+  bold_text1.className = 'note';
+  bold_text1.innerHTML = 'Note:';
+  span_text.appendChild(bold_text1);
 
-    let text_node = doc.createTextNode(' For this demo use following username and password.');
-    span_text.appendChild(text_node);
+  let text_node = doc.createTextNode(' For this demo use following username and password.');
+  span_text.appendChild(text_node);
 
-    let br1 = doc.createElement('br');
-    span_text.appendChild(br1);
+  let br1 = doc.createElement('br');
+  span_text.appendChild(br1);
 
-    let bold_text2 = doc.createElement('b');
-    bold_text2.className = "valid";
-    span_text.appendChild(bold_text2);
+  let bold_text2 = doc.createElement('b');
+  bold_text2.className = 'valid';
+  span_text.appendChild(bold_text2);
 
-    let text_user = doc.createTextNode("User Name: Smilejke");
-    bold_text2.appendChild(text_user);
+  let text_user = doc.createTextNode('User Name: Smilejke');
+  bold_text2.appendChild(text_user);
 
-    let br2 = doc.createElement('br');
-    bold_text2.appendChild(br2);
+  let br2 = doc.createElement('br');
+  bold_text2.appendChild(br2);
 
-    let text_password = doc.createTextNode("Password : Admin");
-    bold_text2.appendChild(text_password);
-  })();
-
+  let text_password = doc.createTextNode('Password : Admin');
+  bold_text2.appendChild(text_password);
+})();
 
 //===========================================================================> page 1
 
 let main = doc.createElement('main');
-main.classList.add("main-div");
-main.id = "main_content_div";
-main.className = "visibility";
+main.classList.add('main-div');
+main.id = 'main_content_div';
+main.className = 'visibility';
 doc.body.append(main);
 
 let work_div = doc.createElement('div');
-work_div.classList.add("button-div");
+work_div.classList.add('button-div');
 main.appendChild(work_div);
 
 let button = doc.createElement('button');
-button.classList.add("new_day");
+button.classList.add('new_day');
 button.innerHTML = 'Добавить рабочий день';
 work_div.appendChild(button);
 
@@ -185,31 +183,62 @@ let create_new_button = (hash) => {
   hash.placeToAppend.appendChild(buttonName);
 };
 
-
 let makeNewRow = () => {
   let input_div = doc.createElement('div');
-  input_div.classList.add("form-div");
+  input_div.classList.add('form-div');
   input_div.id = 'input_div' + result.counter;
   result.input_div_id.push(input_div.id);
   work_div.before(input_div);
 
-  create_new_input({type_name:'input', class_name:"input-date", id_name:"number", placeToPushId:result.number_id, placeholder_name:result.counter, readOnlyParam:true, placeToAppend:input_div});
-  create_new_input({type_name:'input', class_name:"input-date", id_name:"date", placeToPushId:result.date_id, placeholder_name:"Дата", readOnlyParam:false, placeToAppend:input_div});
-  create_new_input({type_name:'input', class_name:"input-date", id_name:"ivr", placeToPushId:result.ivr_id, placeholder_name:"ИВР", readOnlyParam:false, placeToAppend:input_div});
-  create_new_input({type_name:'input', class_name:"input-date", id_name:"hours", placeToPushId:result.hours_id, placeholder_name:"Часы", readOnlyParam:false, placeToAppend:input_div});
+  create_new_input({
+    type_name: 'input',
+    class_name: 'input-date',
+    id_name: 'number',
+    placeToPushId: result.number_id,
+    placeholder_name: result.counter,
+    readOnlyParam: true,
+    placeToAppend: input_div,
+  });
+  create_new_input({
+    type_name: 'input',
+    class_name: 'input-date',
+    id_name: 'date',
+    placeToPushId: result.date_id,
+    placeholder_name: 'Дата',
+    readOnlyParam: false,
+    placeToAppend: input_div,
+  });
+  create_new_input({
+    type_name: 'input',
+    class_name: 'input-date',
+    id_name: 'ivr',
+    placeToPushId: result.ivr_id,
+    placeholder_name: 'ИВР',
+    readOnlyParam: false,
+    placeToAppend: input_div,
+  });
+  create_new_input({
+    type_name: 'input',
+    class_name: 'input-date',
+    id_name: 'hours',
+    placeToPushId: result.hours_id,
+    placeholder_name: 'Часы',
+    readOnlyParam: false,
+    placeToAppend: input_div,
+  });
 
   let remove_button = doc.createElement('button');
   remove_button.id = 'remove' + result.counter;
   result.remove_id.push(remove_button.id);
   remove_button.classList.add('icon-btn', 'add-btn');
   remove_button.addEventListener('click', function() {
-      main.removeChild(this.parentNode);
+    main.removeChild(this.parentNode);
   });
   input_div.appendChild(remove_button);
 
   let remove_div = doc.createElement('div');
   remove_div.classList.add('btn-txt');
-  remove_div.innerHTML = "Удалить";
+  remove_div.innerHTML = 'Удалить';
   remove_button.appendChild(remove_div);
 
   result.counter += 1;
@@ -240,15 +269,27 @@ let get_data = () => {
 let createForwardButton = () => {
   if (result.marker) {
     let last_div = doc.createElement('div');
-    last_div.classList.add("last-div");
+    last_div.classList.add('last-div');
     main.appendChild(last_div);
 
-  
-    create_new_button({type:'button', id:"backButton", class_name:"forward", disabled:true, text:'Предыдущий показатель', placeToAppend:last_div});
-    create_new_button({type:'button', id:"forwardButton", class_name:"forward", disabled:false, text:'Следующий показатель', placeToAppend:last_div});
+    create_new_button({
+      type: 'button',
+      id: 'backButton',
+      class_name: 'forward',
+      disabled: true,
+      text: 'Предыдущий показатель',
+      placeToAppend: last_div,
+    });
+    create_new_button({
+      type: 'button',
+      id: 'forwardButton',
+      class_name: 'forward',
+      disabled: false,
+      text: 'Следующий показатель',
+      placeToAppend: last_div,
+    });
     let getNextButton = doc.getElementById('forwardButton');
     getNextButton.addEventListener('click', get_data);
-
   }
   result.marker = false;
 };
@@ -262,47 +303,69 @@ let makeSomeNoise = () => {
   console.log(result.total_hours);
 };
 
-let = create_new_day = () => {
+let create_new_day = () => {
   makeNewRow();
   createForwardButton();
 };
 
-button.addEventListener("click", create_new_day);
-
+button.addEventListener('click', create_new_day);
 
 let make_kk_table = () => {
   result.counter = 1;
   result.marker = true;
 
-  let get_div_and_make_visible = doc.getElementById("main_content_div");
-  get_div_and_make_visible.classList.add("visibility");
+  let get_div_and_make_visible = doc.getElementById('main_content_div');
+  get_div_and_make_visible.classList.add('visibility');
 
   let make_kk_div2 = doc.createElement('div');
-  make_kk_div2.id = "main_content_div2";
+  make_kk_div2.id = 'main_content_div2';
   doc.body.append(make_kk_div2);
 
   let button_div = doc.createElement('div');
-  button_div.classList.add("button-div");
+  button_div.classList.add('button-div');
   make_kk_div2.appendChild(button_div);
 
   let button = doc.createElement('button');
-    button.classList.add("new_day");
-    button.innerHTML = 'Добавить рабочий день';
-    button.disabled = true;
-    button_div.appendChild(button);
+  button.classList.add('new_day');
+  button.innerHTML = 'Добавить рабочий день';
+  button.disabled = true;
+  button_div.appendChild(button);
 
   for (let i in result.input_div_id) {
     if (i < result.input_div_id.length) {
       let input_div = doc.createElement('div');
-      input_div.classList.add("form-div");
+      input_div.classList.add('form-div');
       input_div.id = 'input_div' + result.counter;
       result.kk_input_div_id.push(input_div.id);
       button_div.before(input_div);
 
-      create_new_input({type_name:'input', class_name:"input-date", id_name:"number", placeToPushId:result.number_id, placeholder_name:result.counter, readOnlyParam:true, placeToAppend:input_div});
-      create_new_input({type_name:'input', class_name:"input-date", id_name:"date", placeToPushId:result.date_id, placeholder_name:"Дата", readOnlyParam:false, placeToAppend:input_div});
-      create_new_input({type_name:'input', class_name:"input-date", id_name:"kk", placeToPushId:result.kk_id, placeholder_name:"КК", readOnlyParam:false, placeToAppend:input_div});
-
+      create_new_input({
+        type_name: 'input',
+        class_name: 'input-date',
+        id_name: 'number',
+        placeToPushId: result.number_id,
+        placeholder_name: result.counter,
+        readOnlyParam: true,
+        placeToAppend: input_div,
+      });
+      create_new_input({
+        type_name: 'input',
+        class_name: 'input-date',
+        id_name: 'date',
+        placeToPushId: result.date_id,
+        placeholder_name: 'Дата',
+        readOnlyParam: false,
+        placeToAppend: input_div,
+      });
+      create_new_input({
+        type_name: 'input',
+        class_name: 'input-date',
+        id_name: 'kk',
+        placeToPushId: result.kk_id,
+        placeholder_name: 'КК',
+        readOnlyParam: false,
+        placeToAppend: input_div,
+      });
 
       let remove_button = doc.createElement('button');
       remove_button.id = 'remove' + result.counter;
@@ -312,19 +375,33 @@ let make_kk_table = () => {
 
       let remove_div = doc.createElement('div');
       remove_div.classList.add('btn-txt');
-      remove_div.innerHTML = "Удалить";
+      remove_div.innerHTML = 'Удалить';
       remove_button.appendChild(remove_div);
 
       result.counter += 1;
     }
-    
+
     if (result.marker) {
       let last_div = doc.createElement('div');
-      last_div.classList.add("last-div");
+      last_div.classList.add('last-div');
       make_kk_div2.appendChild(last_div);
-  
-      create_new_button({type:'button', id:"backButton", class_name:"forward", disabled:false, text:'Предыдущий показатель', placeToAppend:last_div});
-      create_new_button({type:'button', id:"forwardButton", class_name:"forward", disabled:false, text:'Следующий показатель', placeToAppend:last_div});
+
+      create_new_button({
+        type: 'button',
+        id: 'backButton',
+        class_name: 'forward',
+        disabled: false,
+        text: 'Предыдущий показатель',
+        placeToAppend: last_div,
+      });
+      create_new_button({
+        type: 'button',
+        id: 'forwardButton',
+        class_name: 'forward',
+        disabled: false,
+        text: 'Следующий показатель',
+        placeToAppend: last_div,
+      });
     }
     result.marker = false;
   }
